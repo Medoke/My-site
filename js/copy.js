d214b400b29e44e9dc12c7d7636b7533016f6671
Medok <medokes@gmail.com>
2022-01-17 02:38:02 +0200

    Added favicon and other icons, changed contacts' appearances.

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+function Clipboard_CopyTo(value) {
+    var tempInput = document.createElement("input");
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
+
+document.querySelector('#Copy').onclick = function() {
+    Clipboard_CopyTo('Text to copy!');
+}
