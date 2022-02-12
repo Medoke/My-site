bb6bb678f31961eee570062ffb95c386d2cc42ce
Medok <medokes@gmail.com>
2022-02-13 01:26:44 +0200

    made copy work, added JS skill, lightened favicon

--- js/copy.js
+++ js/copy.js
@@ -1,12 +1,5 @@
-function Clipboard_CopyTo(value) {
-    var tempInput = document.createElement("input");
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-}
-
-document.querySelector('#Copy').onclick = function() {
-    Clipboard_CopyTo('Text to copy!');
-}
+const copyToClipboardAsync = str => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+    return navigator.clipboard.writeText(str);
+  return Promise.reject('The Clipboard API is not available.');
+};
